97d1402213381910d2ea41192881e7011357bbc5
Anvar571 <abdurashidovanvar52@gmail.com>
2023-03-08 18:37:40 +0500

    initial commit

---
+++ ts/main.ts
@@ -0,0 +1,22 @@
+import Parser from "./analysis/parser.ts";
+
+repl();
+
+function repl() {
+  const parser = new Parser();
+  console.log("\nRepl v0.1");
+
+  // Continue Repl Until User Stops Or Types `exit`
+  while (true) {
+    const input = prompt("> ");
+    console.clear();
+    // Check for no user input or exit keyword.
+    if (!input || input.includes("exit")) {
+      Deno.exit(1);
+    }
+
+    // Produce AST From sourc-code
+    const program = parser.produceAST(input);
+    console.log(program);
+  }
+}
